a83977b0b05b2c204194198d61fe0d67d5d728d4
chaitanya <ramachaitanya0@gmail.com>
2023-11-14 13:26:38 +0530

    Added auto delete of uploaded files and directory

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import shutil
+
 import streamlit as st
 import os
 from dotenv import load_dotenv
@@ -13,21 +15,46 @@ print("Streamlit run has started")
 # Title
 st.title("Ask PDF")
 
+def create_folder(folder_path):
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
 
-target_dir = "./uploaded_data"
-try :
-    for file in os.listdir(target_dir):
-        os.remove(os.path.join(target_dir + "/" + file))
-except :
-    print("Error in accessing the target directory ")
+def delete_files_in_folder(folder_path):
+    files = os.listdir(folder_path)
+    for file in files:
+        file_path = os.path.join(folder_path, file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+        except Exception as e:
+            st.error(f"Error deleting file {file_path}: {e}")
+
+def delete_folder(folder_path):
+    try:
+        os.rmdir(folder_path)
+    except Exception as e:
+        st.error(f"Error deleting folder {folder_path}: {e}")
+
+
+TARGET_DIR = "./uploaded_data"
 
 # Uploading Files
 uploaded_files = st.file_uploader("Upload your files", type=['pdf'], accept_multiple_files=True)
 
-
 @st.cache_resource
 def load_uploaded_files(uploaded_files: list,target_dir:str):
+    # delete_files_in_folder("./docs")
+    # delete_folder("./docs")
+    # shutil.rmtree("./docs")
+    # try :
+    #     os.remove("./docs/chroma/chroma.sqlite3")
+    # except :
+    #     print("No such directory")
     if len(uploaded_files) > 0:
+        # creates new folder for the uploaded data
+        create_folder(target_dir)
+        print("Created the directory for the uploaded files")
+
         for uploaded_file in uploaded_files:
             file_path = os.path.join(target_dir, uploaded_file.name)
             with open(file_path, "wb") as f:
@@ -42,25 +69,27 @@ def load_uploaded_files(uploaded_files: list,target_dir:str):
             docs.extend(loader.load())
         print(f"length of the docs {len(docs)}")
 
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100,separators=["\n\n","\n"])
         splits = text_splitter.split_documents(docs)
+
         print(f"length of the splits {len(splits)}")
         embedding = OpenAIEmbeddings()
         print("loaded the Open AI Embeddings Function")
 
-        # Deleting the previous content
-        # shutil.rmtree("./docs/chroma/")
-        # print("Deleted the db")
 
-        persist_directory = 'docs/chroma/'
+        # Creating Vector Database and persisting it
+        persist_directory = './docs/chroma/'
         vectordb = Chroma.from_documents(
             documents=splits,
             embedding=embedding,
             persist_directory=persist_directory
         )
+        # Creating a retriever
         retriever = vectordb.as_retriever()
-
         print("Created Vector DB ")
+
+        # Selecting a Model
         current_date = datetime.datetime.now().date()
         if current_date < datetime.date(2023, 9, 2):
             llm_name = "gpt-3.5-turbo-0301"
@@ -69,10 +98,16 @@ def load_uploaded_files(uploaded_files: list,target_dir:str):
         print(llm_name)
         print("chose the llm")
 
+        delete_files_in_folder(target_dir)
+        delete_folder(target_dir)
+        print("Deleted the files and directory")
+
+
         return llm_name, retriever
 
+
 if len(uploaded_files) > 0 :
-    llm_name,retriever = load_uploaded_files(uploaded_files,target_dir=target_dir)
+    llm_name,retriever = load_uploaded_files(uploaded_files,target_dir=TARGET_DIR)
 
     llm = ChatOpenAI(model_name=llm_name, temperature=0)
     chat_history = []
